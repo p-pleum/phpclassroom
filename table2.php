bf2cc31fa091cc01f3a73731d4cad1c33d2ddc11
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-11-16 09:44:02 +0700

    Update table2.php

--- table2.php
+++ table2.php
@@ -1,20 +1,20 @@
 <html>
   <head></head>
   <body>
-     <table border="1">
-       <?php
-       $row = 100;
-       for($i=0;$i<$row;$i++){
-         echo "<tr>";
-         echo "<td>คอลัมน์ที่ 1</td>";
-         echo "<td>คอลัมน์ที่ 2</td>";
-         echo "<td>คอลัมน์ที่ 3</td>";
-         echo "<td>คอลัมน์ที่ 4</td>";
-         echo "<td>คอลัมน์ที่ 5</td>";
-         echo "<td>คอลัมน์ที่ 6</td>";
-         echo "</tr>";
-       }
-    ?>
+    <table border="1">
+      <?php
+        $row = 100;
+        for($i=0;$i<$row;$i++){
+          echo "<tr>";
+          echo "<td>column 1</td>";
+          echo "<td>column 2</td>";
+          echo "<td>column 3</td>";
+          echo "<td>column 4</td>";
+          echo "<td>column 5</td>";
+          echo "<td>column 6</td>";
+          echo "</tr>";
+        }
+      ?>
     </table>
   </body>
 </html>
