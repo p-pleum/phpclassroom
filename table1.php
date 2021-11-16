d464394b412a4a44c0f538dfea7071dbb468e444
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-11-16 09:09:49 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<table border=''>"; //tagเปิด
+
+    for($i=0;$i<10;$i++){
+      echo "<tr>";
+      echo "<td>chop pink</td>";
+      echo "<td>chop pink mak</td>";
+      echo "<td>chop pink mak mak</td>";
+      echo "<td>chop pink mak mak mak</td>";
+      echo "</tr>";
+    }
+    echo "</table>"; //tagปิด
+
+?>
