d73065d9386ddc08cd5ddc0aaeb2ad3db5c49efa
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-07 09:33:04 +0700

    Update capi.php

--- capi.php
+++ capi.php
@@ -4,7 +4,7 @@
   echo "วันที่ : ".$data[0]->txn_date."<br>";
   echo "ผู้ป่วยใหม่ : ".$data[0]->new_case."<br>";
   echo "ผู้ป่วยรวม : ".$data[0]->total_case."<br>";
-  echo "ผู้ป่วยใหม่ในประเทศ : ".$data[0]->new_case_excludeabroad"<br>";
+  echo "ผู้ป่วยใหม่ในประเทศ : ".$data[0]->new_case_excludeabroad."<br>";
   echo "ผู้ป่วยรวมในประเทศ : ".$data[0]->total_case_excludeabroad."<br>";
   echo "ผู้เสียชีวิตใหม่ : ".$data[0]->new_death."<br>";
   echo "ผู้เสียชีวิตรวม : ".$data[0]->total_death."<br>";
