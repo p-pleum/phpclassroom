0e5a764b88b34ac299dd3fa493ab0e6a36400bae
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-21 09:59:04 +0700

    Update province.php

--- province.php
+++ province.php
@@ -30,7 +30,18 @@
     echo "<td>";
     echo $new_case_excludeabroad->province;
     echo "</td>";
-    
+    echo "<td>";
+    echo $total_case_excludeabroad->province;
+    echo "</td>";
+    echo "<td>";
+    echo $new_death->province;
+    echo "</td>";
+    echo "<td>";
+    echo $total_death->province;
+    echo "</td>";
+    echo "<td>";
+    echo $update_date->province;
+    echo "</td>";
     
     echo "</tr>";
   }
