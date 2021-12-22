b4567ac5a5fb89ea79dbf94d551e1258d720930c
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-22 10:04:43 +0700

    Update province.php

--- province.php
+++ province.php
@@ -5,7 +5,13 @@
   
   echo "<table border='1'>";
   echo "<colgroup span=”9” width=”100”</colgroup>";
-  echo "<center><tr><td>ลำดับ</td><td>จังหวัด</td></tr></center>";
+  echo "<center><tr><td>ลำดับ</td><td>จังหวัด</td><td><b>ผู้ป่วยใหม่</b></td>
+  <td><b>ผู้ป่วยรวม</b></td>
+  <td><b>ผู้ป่วยใหม่ในประเทศ</b></td>
+  <td><b>ผู้ป่วยรวมในประเทศ</b></td>
+  <td><b>ผู้เสียชีวิตใหม่</b></td>
+  <td><b>ผู้เสียชีวิตรวม</b></td>
+  <td><b>วันที่อัพเดท</b></td></tr></center>";
   foreach($data as $key=>$val){
     echo "<tr>";
     echo "<td>";
