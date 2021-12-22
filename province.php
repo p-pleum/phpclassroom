ccdc3a1870f41b5f0176521e92f14c78a7523ad4
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-22 10:04:03 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,8 +3,9 @@
   $json = file_get_contents("url");
   $data = json_decode($json);
   
-  echo "<center><table border='1'></center>";
-  echo "<tr><td>ลำดับ</td><td>จังหวัด</td></tr>";
+  echo "<table border='1'>";
+  echo "<colgroup span=”9” width=”100”</colgroup>";
+  echo "<center><tr><td>ลำดับ</td><td>จังหวัด</td></tr></center>";
   foreach($data as $key=>$val){
     echo "<tr>";
     echo "<td>";
