a002ebf377729cf611d671c093c4382ed8f54385
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-22 09:41:11 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,7 +3,7 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-  echo "<table border='1'>";
+  echo "<center><table border='1'></center>";
   echo "<colgroup span=”9” width=”100”</colgroup>";
   echo "<tr><td><b>ลำดับ</b></td>
   <td><b>จังหวัด</b></td>
