b1f85672db89405b0500d328230f64f24e82c7f5
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-22 10:16:40 +0700

    Update province.php

--- province.php
+++ province.php
@@ -1,20 +1,19 @@
 <?php
 
-  $json = file_get_contents("url");
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-  echo "<center><table border='1'>";
-  echo "<colgroup span=”9” width=”100”</colgroup>";
-  echo "<tr><td><b>ลำดับ</b></td>
-  <td><b>จังหวัด</b></td>
-  <td><b>ผู้ป่วยใหม่</b></td>
-  <td><b>ผู้ป่วยรวม</b></td>
-  <td><b>ผู้ป่วยใหม่ในประเทศ</b></td>
-  <td><b>ผู้ป่วยรวมในประเทศ</b></td>
-  <td><b>ผู้เสียชีวิตใหม่</b></td>
-  <td><b>ผู้เสียชีวิตรวม</b></td>
-  <td><b>วันที่อัพเดท</b></td></tr>";
-   foreach($data as $key=>$val=>$new_case=>$total_case=>$new_case_excludeabroad=>$total_case_excludeabroad=>$new_death=>$total_death=>$update_date){
+  echo "<table border='0.5'>";
+  echo "<tr><td>ลำดับ</td>
+  <td>จังหวัด</td>
+  <td>ผู้ป่วยใหม่</td>
+  <td>ผู้ป่วยรวม</td>
+  <td>ผู้ป่วยใหม่ในประเทศ</td>
+  <td>ผู้ป่วยรวมในประเทศ</td>
+  <td>ผู้เสียชีวิตใหม่</td>
+  <td>ผู้เสียชีวิตรวม</td>
+  <td>วันที่อัพเดท</td></tr>";
+  foreach($data as $key=>$val){
     echo "<tr>";
     echo "<td>";
     echo ($key+1);
@@ -24,5 +23,5 @@
     echo "</td>";
     echo "</tr>";
   }
-  echo "</center></table>"; 
+  echo "</table>"; 
 ?>
