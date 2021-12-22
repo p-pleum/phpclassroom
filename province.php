0659861b200820797b4507c9a963e10fd00ee92b
p-pleum <89787558+p-pleum@users.noreply.github.com>
2021-12-22 11:44:48 +0700

    Update province.php

--- province.php
+++ province.php
@@ -8,13 +8,13 @@
  echo "<colgroup  width=”90%”</colgroup>";
   echo '<body style="background-color:mintcream">';
   echo "<tr bgcolor ='peachpuff'><td rowspan='2'><b>ลำดับ</b></td>
-  <td><b>จังหวัด</b></td>
-  <td><b>ผู้ป่วยใหม่</b></td>
-  <td><b>ผู้ป่วยรวม</b></td>
-  <td><b>ผู้ป่วยใหม่ในประเทศ</b></td>
-  <td><b>ผู้ป่วยรวมในประเทศ</b></td>
-  <td><b>ผู้เสียชีวิตใหม่</b></td>
-  <td><b>ผู้เสียชีวิตรวม</b></td>
+  <td rowspan='2'><b>จังหวัด</b></td>
+  <td rowspan='2'><b>ผู้ป่วยใหม่</b></td>
+  <td rowspan='2'><b>ผู้ป่วยรวม</b></td>
+  <td rowspan='2'><b>ผู้ป่วยใหม่ในประเทศ</b></td>
+  <td rowspan='2'><b>ผู้ป่วยรวมในประเทศ</b></td>
+  <td rowspan='2'><b>ผู้เสียชีวิตใหม่</b></td>
+  <td rowspan='2'><b>ผู้เสียชีวิตรวม</b></td>
   </tr>";
   foreach($data as $key=>$val){
     echo "<tr>";
