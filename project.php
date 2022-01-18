e022576e0e826da0d156b7c2d59112cc6a2297f5
p-pleum <89787558+p-pleum@users.noreply.github.com>
2022-01-18 09:56:41 +0700

    Update project.php

--- project.php
+++ project.php
@@ -136,6 +136,8 @@
           <!-- Loop -->
           
   <?php   
+          $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  $data = json_decode($json);
           foreach($data as $key=>$val){
      echo "<tr>";
     echo "<td bgcolor ='peachpuff'>";
