d49138e166fd27da7926d7bb4893ae6dc404b12e
p-pleum <89787558+p-pleum@users.noreply.github.com>
2022-01-18 09:44:30 +0700

    Update project.php

--- project.php
+++ project.php
@@ -51,7 +51,7 @@
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="profile.jpg" class="img-fluid rounded-start" alt="...">
+            <img src="/AB4868FA-5169-4774-8B7E-67932FF66346.jpeg" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
